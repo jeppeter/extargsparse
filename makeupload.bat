997d812b87522f92bdb0ef365316b589596d94a5
jeppeter <jeppeter@gmail.com>
2016-09-10 13:01:49 +0800

    fixup bug in makeupload

--- makeupload.bat
+++ makeupload.bat
@@ -1,1 +1,4 @@
-python setup.py register && python setup.py sdist upload
+
+set script_dir=%~dpD0
+set cur_dir=%CD%
+cd %script_dir% && python setup.py register && python setup.py sdist upload && cd %cur_dir% || cd %cur_dir%
--- makeupload.sh
+++ makeupload.sh
@@ -1,1 +1,4 @@
-python setup.py register && python setup.py sdist upload
+
+script_dir=`dirname $0`
+cur_dir=`pwd`
+cd $script_dir && python setup.py register && python setup.py sdist upload && cd $cur_dir || cd $cur_dir
